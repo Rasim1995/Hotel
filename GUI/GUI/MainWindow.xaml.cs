9a3a9ba86ac24053021d91d5e1087c066e83cfec
Rasim1995 <ismagilovrasim1995@gmail.com>
2016-04-24 10:59:43 +0300

    GUI

---
+++ GUI/GUI/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using HotelClassLibrary;
+
+namespace GUI
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            HomeControl h = new HomeControl();
+            this.sc.Content = h;
+			
+            //UserControl1 u = new UserControl1();
+            //Content = u;
+
+        }
+    }
+}
---
+++ GUI/GUI/Window1.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using HotelClassLibrary;
+
+namespace GUI
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class AuthorizationForm : Window
+    {
+        public AuthorizationForm()
+        {
+            InitializeComponent();
+        }
+
+		void Authorization()
+		{
+			try
+			{
+				Hotel.Authorization(txbxLogin.Text, txbxPassword.Password);
+				this.Close();
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		private void btnEnter_Click(object sender, RoutedEventArgs e)
+		{
+			Authorization();
+		}
+	}
+}
